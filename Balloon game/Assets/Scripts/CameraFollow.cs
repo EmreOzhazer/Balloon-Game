2cd774c48084f65e8dd3525304080e17b33d97b0
EmreOzhazer <emreozhazer@hotmail.com>
2022-07-28 13:36:15 +0300

    camera fixed

--- Balloon game/Assets/Scripts/CameraFollow.cs
+++ Balloon game/Assets/Scripts/CameraFollow.cs
@@ -9,6 +9,7 @@ public class CameraFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("camera started");
        // offset = transform.position - player.position;
     }
     private void FixedUpdate()
